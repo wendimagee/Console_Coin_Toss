e2269af872fbe9ebfe701e4eb3eb6ddc9cd36c6e
Wendi <wendimagee@gmail.com>
2020-12-18 23:16:12 -0500

    Added random number generator, but need to have it print the numberOfFlips int of times.

--- Deliverable2/Program.cs
+++ Deliverable2/Program.cs
@@ -1,16 +1,33 @@
 ﻿using System;
+using System.Collections;
+
+
 
 namespace Deliverable2
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.Write("heads or tails? ");
-            string headsOrTailsGuess = Console.ReadLine();
+		static void Main(string[] args)
+		{
+			Console.Write("heads or tails? ");
+			string headsOrTailsGuess = Console.ReadLine();
 
-            Console.Write("Pick a number 1-10 ");
-            int numberOfFlips = int.Parse(Console.ReadLine());
-        }
-    }
+			Console.Write("Pick a number 1-10 ");
+			int numberOfFlips = int.Parse(Console.ReadLine());
+
+			//Random flip generator
+			Random rnd = new Random();
+			
+			if (rnd.Next() % 2 == 0)
+				{
+					Console.WriteLine("heads");
+				}
+			else
+				{
+					Console.WriteLine("tails");
+				}
+			
+
+		}
+	}
 }
