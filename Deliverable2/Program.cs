0f50647cbe03e438ba02c1302a07f386397a3ad3
Wendi <wendimagee@gmail.com>
2020-12-18 22:46:18 -0500

    Add project files.

---
+++ Deliverable2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Deliverable2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("heads or tails? ");
+            string headsOrTailsGuess = Console.ReadLine();
+
+            Console.Write("Pick a number 1-10 ");
+            int numberOfFlips = int.Parse(Console.ReadLine());
+        }
+    }
+}
