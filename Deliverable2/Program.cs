b19d7d76fe3e0818b8ea45167e1cf6184d2e9e2f
Wendi <wendimagee@gmail.com>
2020-12-20 21:19:29 -0500

    Changed the output section of the program to now be readable as input using StringWriter and a new variable consoleOutput

--- Deliverable2/Program.cs
+++ Deliverable2/Program.cs
@@ -1,12 +1,12 @@
 ﻿using System;
-using System.Collections;
-
+using System.Linq;
+using System.IO;
 
 
 namespace Deliverable2
 {
-    class Program
-    {
+	class Program
+	{
 		static void Main(string[] args)
 		{
 			Console.Write("heads or tails? ");
@@ -17,18 +17,23 @@ namespace Deliverable2
 
 			//Random flip generator
 			Random rnd = new Random();
-			for (int i = 0; i < numberOfFlips; i++)
-				{
-				if (rnd.Next() % 2 == 0)
-				{
-					Console.WriteLine("heads");
-				}
-				else
-				{
-					Console.WriteLine("tails");
-				}
-			}
+			using (StringWriter stringWriter = new StringWriter())
 
+				for (int i = 0; i < numberOfFlips; i++)
+				{
+					if (rnd.Next() % 2 == 0)
+					{
+						Console.WriteLine("heads");
+					}
+					else
+					{
+						Console.WriteLine("tails");
+					}
+					
+					string consoleOutput = stringWriter.ToString();
+
+				
+				}
 		}
 	}
 }
